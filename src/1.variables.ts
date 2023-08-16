66cfea394fbc7bc9b8dede044071383dac6448d1
Jamie Williamson <jamie.williamson@depop.com>
2023-08-16 16:33:46 +0100

    Initial commit - varialbes and types

---
+++ src/1.variables.ts
@@ -0,0 +1,19 @@
+// There are 3 keywords for assigning a variable in js, can you return a list with them in?
+export function keywordList() {
+  return [];
+}
+
+// Can you explain why we have the different keywords in comments here?
+/*
+
+*/
+
+// In general, we should assign all variables using which keyword?
+export function variableAssignement() {
+  return '';
+}
+
+// In general, we should avoid which keyword?
+export function avoidVariableAssignment() {
+  return '';
+}
---
+++ src/2.jsTypes.ts
@@ -0,0 +1,92 @@
+// There are 8 datatypes in javascript can you name them all?
+export function datatypesList() {
+  return [];
+}
+
+//What do you think the type of each of the following variables would be?
+// variable: 45
+export function variable1() {
+  return '';
+}
+
+// variable: 7.5
+export function variable2() {
+  return '';
+}
+
+// variable: 'Hello'
+export function variable3() {
+  return '';
+}
+
+// variable: true
+export function variable4() {
+  return '';
+}
+
+// variable: null
+export function variable5() {
+  return '';
+}
+
+// variable: undefined
+export function variable6() {
+  return '';
+}
+
+/* variable:
+{
+    name: 'William Saliba'
+    position: 'cb'
+}
+*/
+export function variable7() {
+  return '';
+}
+
+/* variable: 
+    const test = () => {
+        return 'test'
+    }
+*/
+export function variable8() {
+  return '';
+}
+
+// Do the answers to any of these surprise you?
+
+// Do you think the following comparisons would return true or false?
+// Update the return statements to return the result you expect
+export function comparison1() {
+  const a = 7;
+  const b = 7;
+
+  a === b;
+  return;
+}
+
+export function comparison2() {
+  const a = 7;
+  const b = '7';
+
+  // @ts-ignore
+  a == b;
+  return;
+}
+
+export function comparison3() {
+  const a = 7;
+  const b = '7';
+
+  // @ts-ignore
+  a === b;
+  return;
+}
+
+export function comparison4() {
+  const a = { name: 'Saliba', position: 'cb' };
+  const b = { name: 'Saliba', position: 'cb' };
+  // @ts-ignore
+  a === b;
+  return;
+}
