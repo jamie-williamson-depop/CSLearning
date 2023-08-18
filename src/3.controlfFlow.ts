9ed963af550f8efd9765d3f317125a635021f065
Jamie Williamson <jamie.williamson@depop.com>
2023-08-18 11:08:42 +0100

    Add Control flow tasks

---
+++ src/3.controlfFlow.ts
@@ -0,0 +1,56 @@
+/* This file contains tasks on basic control flow in js
+   Tasks will involve if statements, loops etc
+*/
+
+/*
+    Write a function which whether a given number is even or odd
+*/
+export function evenOrOdd(number: number) {}
+
+/*
+    Rewrite the previous function using a ternary statement
+*/
+export function evenOrOddTernary(number: number) {}
+
+/*
+    Given an array of numbers, return an array with all of the numbers
+    multiplied by 2
+*/
+export function doubleArray(numbers: number[]) {
+  return;
+}
+
+/*
+    Given a string return a reversed string without using any built in reversing functions
+*/
+export function reverseString(string: string) {
+  return;
+}
+
+/*
+    Given a maximum number, write a function to add all numbers until the total reachs the maximum number
+    e.g. for a maximum number 4, we would sum 1, 2 and 3 before stopping and returning 6
+*/
+export function sumToMaximum(maximumNumber: number) {
+  return;
+}
+
+/*
+    Write a function which copies numbers from one array to another, stopping once you have moved the first odd number
+*/
+export function numberCopyUntilOdd(numbers: number[]) {
+  return;
+}
+
+/*
+    Write a function that takes a number, calls the squareNumber function provided
+    and returns the original number and the answer in an array,
+    e.g. for the number 2, we return [2, 4]
+*/
+function squareNumber(number: number) {
+  return number * number;
+}
+
+export function numberSquarer(number: number) {
+  return;
+}
