0207d7bf6cc391526e08ae51bf4389f01f623e3f
Jamie Williamson <jamie.williamson@depop.com>
2023-10-13 14:27:26 +0100

    Add stage 4 challenges

---
+++ src/4.arrays.ts
@@ -0,0 +1,112 @@
+/*
+    This files contains a number of questions to test your understanding
+    of arrays and their features, and challenge you to think in arrays
+*/
+
+/*
+    Implement the following function to create a new array and return it
+*/
+export function createList() {
+  return;
+}
+
+/*
+    Implement the following function to create a new array with a length specified
+    by the parameter 'length', and fills it with 0s
+    e.g. calling createZeroFilledList(3) should return [0,0,0]
+*/
+export function createZeroFilledList(length: number) {
+  return;
+}
+
+/*
+    Implement the following function so it returns the first two elements
+    of any array it is passed.
+    e.g. calling getFirstTwoElements(['a', 'b', 'c', 'd']) should return ['a', 'b']
+*/
+export function getFirstTwoElements(list: any[]) {
+  return;
+}
+
+/*
+    Implement the following function so it returns the final two elements of any
+    array it is passed.
+    e.g. calling getFinalTwoElements(['w', 'x', 'y', 'z']) should return ['y', 'z']
+*/
+export function getFinalTwoElements(list: any[]) {
+  return;
+}
+
+/*
+    Implement the following function so that it returns the number of vowels in a
+    given word passed to it. This implementation should not use any array functions
+    (map, reduce, foreach etc)
+    e.g. calling vowelCount('hello') should return 2
+*/
+export function vowelCount(word: string) {
+  return;
+}
+
+/*
+    Implement the following function so that it returns the number of vowels in a
+    given word passed to it. This implementation should be written using a forEach
+    e.g. calling vowelCountForEach('hello') should return 2
+*/
+export function vowelCountForEach(word: string) {
+  return;
+}
+
+/*
+    Implement the following function so that, given an array of numbers, it returns
+    an array with each of those numbers squared. This should not use any array functions
+    (map, reduce, foreach etc)
+    e.g. calling numberSquarer([1, 2, 3, 4]) should return [1, 4, 9, 16]
+*/
+export function numberSquarer(numberList: number[]) {
+  return;
+}
+
+/*
+    Implement the following function so that, given an array of numbers, it returns
+    an array with each of those numbers squared. This should use a mpa function
+    e.g. calling numberSquarer([1, 2, 3, 4]) should return [1, 4, 9, 16]
+*/
+export function numberSquarerMap(numberList: number[]) {
+  return;
+}
+
+/*
+    Implement the following function so that, given an array of numbers it returns the
+    sum of each number. This should not use any array functions (map, reduce, foreach)
+    e.g. calling summer([1,2,3,4,5]) should return 15
+*/
+export function summer(numberList: number[]) {
+  return;
+}
+
+/*
+    Implement the following function so that, given an array of numbers it returns the
+    sum of each number. This should use a reduce function.
+    e.g. calling summer([1,2,3,4,5]) should return 15
+*/
+export function summerReduce(numberList: number[]) {
+  return;
+}
+
+/*
+  Implement the following function such that it returns true if every value in the supplied
+  list is even, and false otherwise. Use the 'every' array function
+  e.g. calling isAllEven([2,4,6,8]) should return true, calling isAllEven([2,4,6,9]) should return false
+*/
+export function isAllEven(numberList: number[]) {
+  return;
+}
+
+/*
+  Implement the following function such that it returns true if any value in the supplied
+  list is even, and false otherwise. Use the 'every' array function
+  e.g. calling isAnyEven([1,3,5,8]) should return true, calling isAnyEven([1,3,5,9]) should return false
+*/
+export function isAnyEven(numberList: number[]) {
+  return;
+}
